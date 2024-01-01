5ceada61229a4b5e68a3f574a2520fb3d9cdcb45
BJR <146668433+BJR-Eagle@users.noreply.github.com>
2024-01-01 03:55:51 -0300

    Add files via upload

---
+++ arq1.java
@@ -0,0 +1,22 @@
+public class arq1 {
+    public static void main(String[] args) {
+
+        //Declarar uma variavel int (inteira)
+        int a=10;
+
+        //Declarar uma variavel double (decimal)
+        double b=10.5;
+
+        //Declarar uma variavel string (texto)
+        String c="Junior é incrivel!!!";
+
+        //declarar uma variavel boolean (binaria(true/false))
+        boolean d=true;
+
+        //imprimir as variaveis na tela
+        System.out.println("O Valor inteiro é: "+a);
+        System.out.println("O Valor decimal é: "+b);
+        System.out.println("O Valor String é: "+c);
+        System.out.println("O boolean é: "+d);
+    }
+}
